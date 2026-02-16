778fe330a9fc070552971b905d87cdac66abfc67
Afraz <afraz.tazim@gmail.com>
2026-02-16 23:17:41 +0600

    course update

--- src/course.h
+++ src/course.h
@@ -2,4 +2,31 @@
 
 using namespace std;
 
-class Course {};
+class Course
+{
+private:
+    string courseID;
+    string Title;
+    string credits;
+    string FacultyName;
+    string schedule;
+
+public:
+    Course(string courseID, string Title, string credits, string FacultyName);
+
+    string getCourseID() const;
+
+    void setCourseID(const string &courseID);
+
+    string getTitle() const;
+
+    void setTitle(const string &Title);
+
+    string getCredits() const;
+
+    void setCredits(const string &credits);
+
+    string getFacultyName() const;
+
+    void setFacultyName(const string &FacultyName);
+};
