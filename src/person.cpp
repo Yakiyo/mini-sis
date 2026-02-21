dbfc29da3a1593996d67fd8608c3d46f8fbd8145
Nafi Abrar <nafiabrar@iut-dhaka.edu>
2026-02-21 13:49:24 +0600

    person class implementation

--- src/person.cpp
+++ src/person.cpp
@@ -1,15 +1,24 @@
 #include "person.h"
 
-Person::Person(int id, const std::string& name) : id(id), name(name) {}
+using namespace std;
+
+Person::Person(int id, const string name){
+    this->id = id;
+    this->name = name;
+}
 
 int Person::getId() const {
     return id;
 }
 
-std::string Person::getName() const {
+void Person::setId(int id){
+    this->id = id;
+}
+
+string Person::getName() const {
     return name;
 }
 
-std::string Person::type() const {
-    return "Person";
+void Person::setName(const string name){
+    this->name = name;
 }
