0bc2d0d5484c793299ed4efb0cedbdf88de503a0
Yakiyo <yakiyo1206@gmail.com>
2026-03-27 19:03:05 +0600

    wallet

---
+++ src/model/wallet.cpp
@@ -0,0 +1,51 @@
+#include "wallet.h"
+
+#include <string>
+#include <vector>
+
+#include "../error/error.h"
+#include "../interface/serde.h"
+
+using namespace std;
+
+Wallet::Wallet(int studentId, double balance) : studentId(studentId), balance(balance) {}
+
+string Wallet::serialize() const {
+    return to_string(studentId) + "," + to_string(balance);
+}
+
+Wallet Wallet::deserialize(const string& data) {
+    vector<string> parts = strsplit(data, ',');
+    if (parts.size() != 2) {
+        throw DsrlzExcept("Wallet", data);
+    }
+    int studentId = stoi(parts[0]);
+    double balance = stod(parts[1]);
+    return Wallet(studentId, balance);
+}
+
+int Wallet::getStudentId() const {
+    return studentId;
+}
+
+double Wallet::getBalance() const {
+    return balance;
+}
+
+Wallet& Wallet::operator+(const int amount) {
+    this->balance += amount;
+    return *this;
+}
+
+Wallet& Wallet::operator-(const int amount) {
+    this->balance -= amount;
+    return *this;
+}
+
+// friend function
+Wallet& operator-(const int amount,  Wallet& self) {
+    self.balance -= amount;
+    return self;
+}
+
+
---
+++ src/model/wallet.h
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+#include "../interface/serde.h"
+
+using namespace std;
+
+class Wallet : public Serde<Wallet> {
+   private:
+    int studentId;
+    double balance;
+
+   public:
+    Wallet(int studentId, double balance);
+
+    string serialize() const override;
+    static Wallet deserialize(const string& data);
+
+    int getStudentId() const;
+    double getBalance() const;
+
+    Wallet& operator+(const int amount);
+
+    friend Wallet& operator-(const int amount, Wallet& self);
+    Wallet& operator-(const int amount);
+};
